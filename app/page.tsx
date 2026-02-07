5c933db465942e9cd7fdf49f111bf98cc3bd836a
Aryan Mehta <aryannmehta@gmail.com>
2026-02-07 04:10:14 -0500

    implement basic header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image';
 import React from 'react'
 
 const Home = () => {
   return (
-    <div></div>
+    <main className='main-container'>
+      <section className='home-grid'>
+        <div id='coin-overview'>
+          <div className='header pt-2'>
+            <Image src='https://assets.coingecko.com/coins/images/1/large/bitcoin.png' alt='Bitcoin Logo' width={56} height={56} />
+            <div className='info'>
+              <p>Bitcoin / BTC</p>
+              <h1>$89,113.00</h1>
+            </div>
+          </div>
+        </div>
+
+        <p>Trending Coins</p>
+      </section>
+
+      <section className='w-full mt-7 space-y-4'>
+        <p>Categories</p>
+      </section>
+    </main>
   );
 }
 

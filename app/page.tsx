e22afd1a38e3a8f74b7828a7bc21f311c05a8f36
Aryan Mehta <aryannmehta@gmail.com>
2026-02-07 06:32:49 -0500

    build tables with data from api, add suspense, add candlestick ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,75 +1,29 @@
-import DataTable from '@/components/DataTable';
-import { cn } from '@/lib/utils';
-import { TrendingDown, TrendingUp } from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { Suspense } from 'react';
+import CoinOverview from '@/components/home/CoinOverview';
+import TrendingCoins from '@/components/home/TrendingCoins';
+import { CategoriesFallback, CoinOverviewFallback, TrendingCoinsFallback } from '@/components/home/fallback';
+import Categories from '@/components/home/Categories';
 
-const columns: DataTableColumn<TrendingCoin>[] = [
-  { 
-    header: 'Name',
-    cellClassName: 'name-cell',
-    cell: (coin) => {
-      const item = coin.item;
-
-      return (
-        <Link href={`/coins/${item.id}`}>
-          <Image src={item.large} alt={item.name} width={36} height={36} />
-          <p>{item.name}</p>
-        </Link>
-      )
-    }
-  },
-  {
-    header: '24h Change',
-    cellClassName: 'name-cell',
-    cell: (coin) => {
-      const item = coin.item;
-      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
-
-      return (
-        <div className={cn('price-change', isTrendingUp ? 'text-green-500' : 'text-red-500')}>
-          <p>
-            {isTrendingUp ? (
-              <TrendingUp width={16} height={16} />
-            ) : (
-              <TrendingDown width={16} height={16} />
-            )}
-            {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
-          </p>
-        </div>
-      )
-    }
-  }, 
-  {
-    header: 'Price',
-    cellClassName: 'price-cell',
-    cell: (coin) => coin.item.data.price
-  },
-];
-
-const Home = () => {
+const Page = async () => {
   return (
-    <main className='main-container'>
-      <section className='home-grid'>
-        <div id='coin-overview'>
-          <div className='header pt-2'>
-            <Image src='https://assets.coingecko.com/coins/images/1/large/bitcoin.png' alt='Bitcoin Logo' width={56} height={56} />
-            <div className='info'>
-              <p>Bitcoin / BTC</p>
-              <h1>$89,113.00</h1>
-            </div>
-          </div>
-        </div>
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
 
-        <p>Trending Coins</p>
-        <DataTable data={[]} columns={[]} />
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
       </section>
 
-      <section className='w-full mt-7 space-y-4'>
-        <p>Categories</p>
+      <section className="w-full mt-7 space-y-4">
+        <Suspense fallback={<CategoriesFallback />}>
+          <Categories />
+        </Suspense>
       </section>
     </main>
   );
-}
+};
 
-export default Home;
+export default Page;
